ff50f9413f13eb4045ca74903a8009840b5af0af
akshayApplore <akshaysharmaw@gmail.com>
2023-08-08 18:25:36 +0530

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,67 @@
+## 0.1.4
+* chore: export editor style ([#27](https://github.com/AppFlowy-IO/appflowy-editor/pull/25)).
+
+## 0.1.3
+* chore: update the dependencies to the latest release by @LucasXu0 ([#25](https://github.com/AppFlowy-IO/appflowy-editor/pull/25)).
+
+## 0.1.2
+* fix: bug on node iterator with nested nodes by @LucasXu0 ([#11](https://github.com/AppFlowy-IO/appflowy-editor/pull/11)).
+* fix: SVG display error in the latest version by @LucasXu0 ([#12](https://github.com/AppFlowy-IO/appflowy-editor/pull/12)).
+* fix: Using the mouse to highlight text very easy to miss the first letter by @LucasXu0 ([#13](https://github.com/AppFlowy-IO/appflowy-editor/pull/13)).
+* chore: fix some known issues by @LucasXu0 in ([#14](https://github.com/AppFlowy-IO/appflowy-editor/pull/14)).
+
+## 0.1.1
+* Relicense Appflowy Editor by @hyj1204.
+* Improve the toolbar user-experience by @LucasXu0.
+* Improve the test code coverage by @GouravShDev.
+
+## 0.1.0
+* Support Flutter 3.7.5.
+
+## 0.0.9
+* Support customize the text color and text background color.
+* Fix some bugs.
+
+## 0.0.8
+* Fix the toolbar display issue.
+* Fix the copy/paste issue on Windows.
+* Minor Updates.
+
+## 0.0.7
+* Refactor theme customizer, and support dark mode.
+* Support export and import markdown.
+* Refactor example project.
+* Fix some bugs.
+
+## 0.0.6
+* Add three plugins: Code Block, LateX, and Horizontal rule.
+* Support web platform.
+* Support more markdown syntax conversions.
+    * `~ ~` to format text as strikethrough
+    * `_ _` to format text as italic
+    * \` \` to format text as code
+    * `[]()` to format text as link
+* Fix some bugs.
+
+## 0.0.5
+* Support customize the hotkeys for a shortcut on different platforms.
+* Support customize a theme.
+* Support localizations.
+* Support insert numbered lists.
+* Fix some bugs.
+
+## 0.0.4
+* Support more shortcut events.
+* Fix some bugs.
+* Update the documentation.
+
+## 0.0.3
+* Support insert image.
+* Support insert link.
+* Fix some bugs.
+
+## 0.0.2
+Minor Updates to Documentation.
+
+## 0.0.1
+Initial Version of the library.
---
+++ test/extensions/node_extension_test.dart
@@ -0,0 +1,121 @@
+import 'dart:collection';
+
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../infra/test_editor.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNode extends Mock implements Node {}
+
+void main() {
+  group('node_extension.dart', () {
+    final selection = Selection(
+      start: Position(path: [0]),
+      end: Position(path: [1]),
+    );
+
+    test('inSelection', () {
+      // I use an empty implementation instead of mock, because the mocked
+      // version throws error trying to access the path.
+
+      final subLinkedList = LinkedList<Node>()
+        ..addAll([
+          Node(type: 'type', children: LinkedList(), attributes: {}),
+          Node(type: 'type', children: LinkedList(), attributes: {}),
+          Node(type: 'type', children: LinkedList(), attributes: {}),
+          Node(type: 'type', children: LinkedList(), attributes: {}),
+          Node(type: 'type', children: LinkedList(), attributes: {}),
+        ]);
+
+      final linkedList = LinkedList<Node>()
+        ..addAll([
+          Node(
+            type: 'type',
+            children: subLinkedList,
+            attributes: {},
+          ),
+        ]);
+
+      final node = Node(
+        type: 'type',
+        children: linkedList,
+        attributes: {},
+      );
+      final result = node.inSelection(selection);
+      expect(result, false);
+    });
+
+    test('inSelection w/ Reverse selection', () {
+      final linkedList = LinkedList<Node>()
+        ..addAll([
+          Node(
+            type: 'type',
+            attributes: {},
+          ),
+        ]);
+
+      final node = Node(
+        type: 'type',
+        children: linkedList,
+        attributes: {},
+      );
+
+      final reverseSelection = Selection(
+        start: Position(path: [1]),
+        end: Position(path: [0]),
+      );
+
+      final result = node.inSelection(reverseSelection);
+      expect(result, false);
+    });
+
+    testWidgets('isSelected', (tester) async {
+      final editor = tester.editor
+        ..insertTextNode('Hello')
+        ..insertTextNode('World');
+      await editor.startTesting();
+
+      final selection = Selection(
+        start: Position(path: [0]),
+        end: Position(path: [0], offset: 1),
+      );
+
+      await editor.updateSelection(selection);
+
+      final node = editor.editorState.getTextNode(path: [0]);
+
+      expect(node.isSelected(editor.editorState), true);
+    });
+
+    testWidgets('insert a new checkbox after an exsiting checkbox',
+        (tester) async {
+      const text = 'Welcome to Appflowy 😁';
+      final editor = tester.editor
+        ..insertTextNode(
+          text,
+        )
+        ..insertTextNode(
+          text,
+        )
+        ..insertTextNode(
+          text,
+        );
+      await editor.startTesting();
+      final selection = Selection(
+        start: Position(path: [2], offset: 5),
+        end: Position(path: [0], offset: 5),
+      );
+      await editor.updateSelection(selection);
+      final nodes =
+          editor.editorState.service.selectionService.currentSelectedNodes;
+      expect(
+        nodes.map((e) => e.path).toList().toString(),
+        '[[2], [1], [0]]',
+      );
+      expect(
+        nodes.normalized.map((e) => e.path).toList().toString(),
+        '[[0], [1], [2]]',
+      );
+    });
+  });
+}
